ca38cdb42ccdc86f688ab1c3980760af892d4af9
OsamaMkoderlabs <muhammad.osama@koderlabs.com>
2022-12-02 15:31:48 +0500

    init

---
+++ index.js
@@ -0,0 +1,81 @@
+import { config } from "./config";
+
+const file = document.getElementById("input-media");
+const upload = document.getElementById("upload");
+
+upload.addEventListener("click", async () => {
+  let selectFile = file.files[0];
+
+  const fr = new FileReader();
+  fr.readAsArrayBuffer(selectFile);
+
+  fr.onload = async function () {
+    let blob = new Blob([fr.result]);
+    try {
+      const res = await fetch(`${config.BASE_URL}/media/init`, {
+        method: "POST",
+        headers: {
+          Authorization: config.AUTH_TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: "string.jpg",
+          size: 10,
+          type: "IMAGE",
+          public: false,
+        }),
+      });
+
+      const credentials = await res.json();
+
+      const uploadedItem = await uploadOnS3(blob, credentials);
+
+      await fetch(`${config.BASE_URL}/media/finalize`, {
+        method: "POST",
+        headers: {
+          Authorization: config.AUTH_TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: Number(credentials.mediaId),
+        }),
+      });
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  const uploadOnS3 = async (stream, credentials, cb) => {
+    if (!window.AWS) {
+      return;
+    }
+
+    window.AWS.config.update({ region: credentials.region });
+
+    let s3 = new window.AWS.S3({
+      credentials: new window.AWS.Credentials({
+        accessKeyId: credentials.accessKeyId,
+        secretAccessKey: credentials.secretAccessKey,
+        sessionToken: credentials.sessionToken,
+        signatureVersion: "v4",
+        region: credentials.region,
+      }),
+    });
+
+    let uploadedItem = await s3
+      .upload(
+        {
+          Bucket: credentials.bucket,
+          Key: credentials.location,
+          ACL: "public-read",
+          Body: stream,
+        },
+        function (err, data) {
+          console.log(err, data);
+        }
+      )
+      .promise();
+
+    return uploadedItem;
+  };
+});
